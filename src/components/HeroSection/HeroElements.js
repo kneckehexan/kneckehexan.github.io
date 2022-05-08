02ddeea69f72411fc05f19559e87d523d5288b96
Philip Tunbjer <philip.tunbjer@gmail.com>
2022-05-08 22:29:06 +0200

    Fix: modify pdf

--- src/components/HeroSection/HeroElements.js
+++ src/components/HeroSection/HeroElements.js
@@ -7,7 +7,8 @@ export const HeroContainer = styled.div`
   justify-content: center;
   align-items: center;
   padding: 0 30px;
-  height: 100vh;
+  height: 109vh;
+  width: 100vw;
   position: relative;
   z-index: 1;
   /*margin-top: -10px;*/
